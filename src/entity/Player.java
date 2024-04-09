25d747c41faa6c58e2217cd64763db8bb9b02f21
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-10 02:56:50 +0500

    learning

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -39,11 +39,12 @@ public class Player extends Entity {
             up1 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_up_1.png"));
             up2 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_up_2.png"));
             down1 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_down_1.png"));
-            down2 = ImageIO.read(new File("~/IdeaProjects/My2DGame/Assets/src/boy_down_2.png"));
+            down2 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_down_2.png"));
             left1 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_left_1.png"));
-            left2 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_left_1.png"));
+            left2 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_left_2.png"));
             right1 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_right_1.png"));
             right2 = ImageIO.read(new File("/home/zubair/IdeaProjects/My2DGame/Assets/src/boy_right_2.png"));
+
 
         } catch (IOException e) {
 
@@ -53,22 +54,32 @@ public class Player extends Entity {
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
 
-            direction = "up";
-            y -= speed;
-        } else if (keyH.downPressed == true) {
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
 
-            direction = "down";
-            y += speed;
-        } else if (keyH.leftPressed == true) {
+            if (keyH.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
 
-            direction = "left";
-            x -= speed;
-        } else if (keyH.rightPressed == true) {
-
-            direction = "right";
-            x += speed;
+            spriteCounter++;
+            if (spriteCounter > 12) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
         }
     }
 
@@ -81,19 +92,38 @@ public class Player extends Entity {
 
         switch (direction) {
             case "up":
-                image = up1;
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
                 break;
-
             case "down":
-                image = down1;
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
                 break;
 
             case "left":
-                image = left1;
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
 
             case "right":
-                image = right1;
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
                 break;
         }
 
