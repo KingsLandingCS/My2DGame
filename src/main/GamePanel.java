5a1241bbb560846a31971c493deb0ad4a880e63f
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-04-12 22:03:01 +0500

    learning

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,11 +12,12 @@ public class GamePanel extends JPanel implements Runnable {
 
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
+
     public final int tileSize = originalTileSize * scale; //48x48
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 px
-    final int screenheight = tileSize * maxScreenRow; // 576px
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 px
+    public final int screenheight = tileSize * maxScreenRow; // 576px
 
     // FPS
     int FPS = 60;
